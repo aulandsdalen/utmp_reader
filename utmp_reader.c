b2a43fecadce827fcd1cd907495400af7ce56cc6
aulandsdalen <aulandsdalen@gmail.com>
2015-02-23 12:47:17 +0000

    formatting fix

--- utmp_reader.c
+++ utmp_reader.c
@@ -6,21 +6,31 @@ void print_entry (int num, struct utmp *up) {
     printf("%d\t", num);
     if (up->ut_type)
        printf("%d\t", up->ut_type);
+    else
+       printf("x\t");
     if (up->ut_pid)
        printf("%d\t", up->ut_pid);
-    if (up->ut_user && up->ut_user != "")
-       printf("%s\t", up->ut_user);
+    else
+       printf("xxxx\t");
+    if (up->ut_user)
+       printf("%s\t\t", up->ut_user);
+    else
+       printf("system\t\t");
     if (up->ut_host)
-       printf("%s\t", up->ut_host);
+       printf("%s\t\t", up->ut_host);
+    else
+       printf("hostname\t\t");
     if (up->ut_time)
        printf("%ld\n", up->ut_time);
+    else
+       printf("0000000000\n");
 }
 
 int main(int argc, char** argv) {
     struct utmp *utmp_data;
     int i = 1;
     setutent();
-    printf("#\tut_type\tut_pid\tut_user\t\tut_host\tut_time\n");
+    printf("#\tut_type\tut_pid\tut_user\t\tut_host\t\tut_time\n");
     while(utmp_data = getutent()) {
      print_entry(i, utmp_data);
      ++i;
